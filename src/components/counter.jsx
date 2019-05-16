1593456ca64e3b8b8d427c1d1d85e3ab03ffea93
hiranthaPeiris <hiranthapeiris404@gmail.com>
2019-05-16 23:43:23 +0530

    bootstrap

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,8 +2,16 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <button type="button" class="btn btn-primary">Primary Bootsrap Button</button>
+      </div>
+    );
   }
+  
+  
 }
+
 
 export default Counter;
