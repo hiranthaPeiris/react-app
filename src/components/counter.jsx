d1e5c909bc317147da8b0a777668249ba03a6207
hiranthaPeiris <hiranthapeiris404@gmail.com>
2019-05-18 23:30:27 +0530

    Init#2

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count :0
+  };
   render() {
     return (
       <div>
         <h1>Hello World</h1>
-        <button type="button" class="btn btn-primary">Primary Bootsrap Button</button>
+        <span>{this.formatCode()}</span>
+        <button type="button" className="btn btn-primary">Primary Bootsrap Button</button>
+        <button type="button" className="btn btn-secondary">Cancel</button>
       </div>
     );
   }
   
+  formatCode () {
+    const {count} = this.state;
+    return count === 0 ? "Zero" : count;
+  }
   
 }
 
