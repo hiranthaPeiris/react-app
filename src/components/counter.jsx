ea6b68af272b9e780cd986779983c22e70f6a4fd
hiranthaPeiris <hiranthapeiris404@gmail.com>
2019-05-19 19:09:03 +0530

    init#3

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,25 +2,35 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count :0
+    count: 0,
+    tags : ["tag1","tag2","tag3"]
   };
   render() {
     return (
       <div>
         <h1>Hello World</h1>
-        <span>{this.formatCode()}</span>
-        <button type="button" className="btn btn-primary">Primary Bootsrap Button</button>
-        <button type="button" className="btn btn-secondary">Cancel</button>
+        <span className={this.getClasses()}>{this.formatCode()}</span>
+        <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+        <button type="button" className="btn btn-primary">
+          Primary Bootsrap Button
+        </button>
+        <button type="button" className="btn btn-secondary">
+          Cancel
+        </button>
       </div>
     );
   }
-  
-  formatCode () {
-    const {count} = this.state;
+
+  getClasses() {
+    let classes = "badge badge-pill badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCode() {
+    const { count } = this.state;
     return count === 0 ? "Zero" : count;
   }
-  
 }
-
 
 export default Counter;
