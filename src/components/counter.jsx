6192ab60ca0f9128ec16fd9981b60940be28f5cb
hiranthaPeiris <hiranthapeiris404@gmail.com>
2019-05-20 15:33:49 +0530

    init#4

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,22 +5,36 @@ class Counter extends Component {
     count: 0,
     tags : ["tag1","tag2","tag3"]
   };
+  constructor(){
+    super();
+    this.handleClick = this.handleClick.bind(this);
+    
+  }
   render() {
     return (
       <div>
         <h1>Hello World</h1>
         <span className={this.getClasses()}>{this.formatCode()}</span>
-        <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+        {this.renderTags()}
         <button type="button" className="btn btn-primary">
           Primary Bootsrap Button
         </button>
-        <button type="button" className="btn btn-secondary">
+        <button onClick={this.handleClick} type="button" className="btn btn-secondary">
           Cancel
         </button>
       </div>
     );
   }
+  handleClick(){
+    this.setState({count: this.state.count+1});
+  }
 
+  renderTags(){
+    if(this.state.tags.length===0){
+      return <p>There are no tags</p>
+    }
+    return <ul>{this.state.tags.map(tag =><li key={tag}>{tag}</li>)}</ul>;
+  }
   getClasses() {
     let classes = "badge badge-pill badge-";
     classes += this.state.count === 0 ? "warning" : "primary";
