ad86d20016950900603f84097d6d2c214a96f069
hiranthaPeiris <hiranthapeiris404@gmail.com>
2019-05-26 21:28:20 +0530

    init#9

--- src/components/Counters.jsx
+++ src/components/Counters.jsx
@@ -9,18 +9,37 @@ class Counters extends Component {
       { id: 4, value: 0 }
     ]
   };
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+    console.log(counter);
+  };
+
   handleDelete = counterId => {
     const counters = this.state.counters.filter(c => c.id !== counterId);
     this.setState({ counters: counters });
   };
 
+  handleRestet = () => {
+      const counters = this.state.counters.map(c => {
+          c.value =0;
+          return c;
+      });
+      this.setState({counters});
+  }
+
   render() {
     return (
       <div>
+        <button onClick={this.handleRest} className="btn btn-primary m-2">Reset</button>
         {this.state.counters.map(counter => (
           <Counter
             key={counter.id}
             onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
             counter={counter}
           />
         ))}
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,24 +1,14 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: this.props.counter.value
-  };
 
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
   render() {
-    console.log(this.props);
-
     return (
       <div>
-        {this.props.children}
         <span className={this.getClasses()}>{this.formatCode()}</span>
 
         <button
-          onClick={this.handleClick}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           type="button"
           className="btn btn-secondary m-2"
         >
@@ -28,19 +18,16 @@ class Counter extends Component {
       </div>
     );
   }
-  handleClick() {
-    this.setState({ count: this.state.count + 1 });
-  }
-
+  
   getClasses() {
     let classes = "badge badge-pill badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCode() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
 }
 
