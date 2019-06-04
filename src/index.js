f3deda7818656ef51f08d3e75346b418c93517aa
hiranthaPeiris <hiranthapeiris404@gmail.com>
2019-06-05 03:41:01 +0530

    new#3

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import 'feather-icons/dist/feather'
 
 
 ReactDOM.render(<App/>, document.getElementById('root'));
